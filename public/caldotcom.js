5a86842b5f16de0a649b96707d7d84bbbdfba9de
Alexandre Stahmer <alexandre.stahmer@gmail.com>
2024-02-21 17:14:42 +0100

    maybe

---
+++ public/caldotcom.js
@@ -0,0 +1,38 @@
+;(function (C, A, L) {
+  let p = function (a, ar) {
+    a.q.push(ar)
+  }
+
+  let d = C.document
+  C.Cal =
+    C.Cal ||
+    function () {
+      let cal = C.Cal
+      let ar = arguments
+      if (!cal.loaded) {
+        cal.ns = {}
+        cal.q = cal.q || []
+        d.head.appendChild(d.createElement('script')).src = A
+        cal.loaded = true
+      }
+      if (ar[0] === L) {
+        const api = function () {
+          p(api, arguments)
+        }
+        const namespace = ar[1]
+        api.q = api.q || []
+        typeof namespace === 'string' ? (cal.ns[namespace] = api) && p(api, ar) : p(cal, ar)
+        return
+      }
+      p(cal, ar)
+    }
+})(window, 'https://app.cal.com/embed/embed.js', 'init')
+Cal('init', { origin: 'https://cal.com' })
+
+Cal('floatingButton', {
+  calLink: 'astahmer/consult',
+  buttonColor: 'var(--colors-work)',
+  buttonTextColor: '#ffffff',
+  buttonText: 'Schedule a consultation',
+})
+Cal('ui', { styles: { branding: { brandColor: '#000000' } }, hideEventTypeDetails: false, layout: 'month_view' })
